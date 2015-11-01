dafa41b29adc49ca531024f6a00337e1a78766c9
AlexDux <alexander.dykov@gmail.com>
2015-11-01 19:40:40 +0200

    -Add simple Camera Follow script

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 velocity = Vector3.zero;
+    public float offsetX = 0f;
+    public float offsetY = 2.5f;
+
+    public float smoothing = 0.15f;
+
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x + offsetX, target.position.y + offsetY, transform.position.z), ref velocity, smoothing);
+        }
+    }
+}
